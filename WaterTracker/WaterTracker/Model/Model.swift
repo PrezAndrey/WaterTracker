d4ce7d1e8ae4e55d4a141be402c80fd2e6b645dc
PrezAndrey <aw06.1994@yandex.ru>
2021-12-21 22:52:50 +0300

    View and Model files added

---
+++ WaterTracker/WaterTracker/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  WaterTracker
+//
+//  Created by Андрей През on 21.12.2021.
+//
+
+import Foundation
---
+++ WaterTracker/WaterTracker/View/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  WaterTracker
+//
+//  Created by Андрей През on 21.12.2021.
+//
+
+import Foundation
