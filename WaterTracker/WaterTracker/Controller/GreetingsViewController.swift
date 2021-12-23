d4c4eae4178a0973e650c889f217cb8c253b8eed
PrezAndrey <aw06.1994@yandex.ru>
2021-12-23 09:52:30 +0300

    Screen navigation and some Controllers added

---
+++ WaterTracker/WaterTracker/Controller/GreetingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GreetingsViewController.swift
+//  WaterTracker
+//
+//  Created by Андрей През on 22.12.2021.
+//
+
+import UIKit
+
+class GreetingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Greetings Navigation Controller: \(self.navigationController)")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WaterTracker/WaterTracker/Controller/StatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatsViewController.swift
+//  WaterTracker
+//
+//  Created by Андрей През on 22.12.2021.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Stats Navigation Controller: \(String(describing: self.navigationController))")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
