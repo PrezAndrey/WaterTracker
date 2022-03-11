be880b43047994047f575851cbe047812d120540
AndreyPrez <aw06.1994@yandex.ru>
2022-03-11 10:53:24 +0300

    HealthKitAdapter correction

--- WaterTracker/WaterTracker/Model/HealthKitAdapter.swift
+++ WaterTracker/WaterTracker/Model/HealthKitAdapter.swift
@@ -1,57 +1,38 @@
 //
-//  HealthKitSetup.swift
+//  hk.swift
 //  WaterTracker
 //
-//  Created by Андрей През on 08.02.2022.
+//  Created by Андрей  on 11.03.2022.
 //
 
 import Foundation
 import HealthKit
 
-// Взаимодействие с HK
+// HealthKitAdapter - работает с HealthKit
+
 class HealthKitAdapter {
     
-    enum HealthKitSetupError: Error {
-       case dataIsNotAvailable, deviceNotAvailable
-       
-   }
+     enum HealthKitAdapterError: Error {
+        case dataIsNotAvailable, deviceNotAvailable
+        
+    }
     
     static let shared: HealthKitAdapter = HealthKitAdapter()
+     
+    static let store = HKHealthStore()
+     
+     private let objectTypes: [HKObjectType] = [
+         HKObjectType.quantityType(forIdentifier: .dietaryWater),
+         HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
+         HKObjectType.characteristicType(forIdentifier: .biologicalSex),
+         HKObjectType.characteristicType(forIdentifier: .bloodType)
+     ].compactMap({ $0 })
     
-   static let store = HKHealthStore()
     
-    private let objectTypes: [HKObjectType] = [
-        HKObjectType.quantityType(forIdentifier: .dietaryWater),
-        HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
-        HKObjectType.characteristicType(forIdentifier: .biologicalSex),
-        HKObjectType.characteristicType(forIdentifier: .bloodType)
-    ].compactMap({ $0 })
-    
-    
-//    // writing water to HealthKit
-////    func writeWater(amount: Double) {
-////        guard let waterType = HKSampleType.quantityType(forIdentifier: .dietaryWater) else {
-////                print("Sample type not available")
-////                return
-////            }
-////
-////            let waterQuantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: amount)
-////            let today = Date()
-////            let waterQuantitySample = HKQuantitySample(type: waterType, quantity: waterQuantity, start: today, end: today)
-////
-////            store.save(waterQuantitySample) { (success, error) in
-////                print("HK write finished - success: \(success); error: \(error)")
-//
-//            }
-//    }
-    
-    // Checking authorization status
     func authorizeIfNeeded() {
-//    func checkAuthorization() {
-        
-        
         var typesWithoutAuth: [HKObjectType] = []
         for objectType in objectTypes {
+            
             let status = HealthKitAdapter.store.authorizationStatus(for: objectType)
             
             switch status {
@@ -75,128 +56,50 @@ class HealthKitAdapter {
                 fatalError()
             }
         }
-        
-        // do something with typesWithoutAuth
-        
-        
-    }
     
-    // ask for permission for every data type that we want to read and ask permissions for writing the samples.
-    private func getAuthorization(completion: @escaping (Bool, Error?) -> Void) {
+    
+        // ask for permission for every data type that we want to read and ask permissions for writing the samples.
+        func getAuthorization(completion: @escaping (Bool, Error?) -> Void) {
         
-        guard HKHealthStore.isHealthDataAvailable() else {
-            completion(false, HealthKitSetupError.deviceNotAvailable)
-            return
-        }
+            guard HKHealthStore.isHealthDataAvailable() else {
+                completion(false, HealthKitAdapterError.deviceNotAvailable)
+                return
+            }
         
-        guard
-            let water = HKObjectType.quantityType(forIdentifier: .dietaryWater),
-            let dob = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
-            let sex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
-            let height = HKObjectType.characteristicType(forIdentifier: .height),
-            let weight = HKObjectType.characteristicType(forIdentifier: .weight),
-            let bloodType = HKObjectType.characteristicType(forIdentifier: .bloodType)
-        else {
-            completion(false, HealthKitSetupError.dataIsNotAvailable)
+            guard
+                let water = HKObjectType.quantityType(forIdentifier: .dietaryWater),
+                let dob = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
+                let sex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
+                let bloodType = HKObjectType.characteristicType(forIdentifier: .bloodType)
+            else {
+                completion(false, HealthKitAdapterError.dataIsNotAvailable)
             
-            return
-        }
+                return
+            }
         
-        let writing: Set<HKSampleType> = [water]
-        let reading: Set<HKObjectType> = [water, dob, sex, bloodType]
-        
-        HKHealthStore().requestAuthorization(toShare: writing, read: reading, completion: completion)
-       
-        
-        
+            let writing: Set<HKSampleType> = [water]
+            let reading: Set<HKObjectType> = [water, dob, sex, bloodType]
+            
+            HKHealthStore().requestAuthorization(toShare: writing, read: reading, completion: completion)
     }
-    
+      
+        
+        
+    // writing water to HealthKit
+    func writeWater(amount: Double) {
+        guard let waterType = HKSampleType.quantityType(forIdentifier: .dietaryWater) else {
+                print("Sample type not available")
+                return
+            }
+            
+            let waterQuantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: amount)
+            let today = Date()
+            let waterQuantitySample = HKQuantitySample(type: waterType, quantity: waterQuantity, start: today, end: today)
+            
+            HKHealthStore().save(waterQuantitySample) { (success, error) in
+                print("HK write finished - success: \(success); error: \(error)")
+                
+            }
+    }
+    }
 }
-
-
-//class HealthKitSetup {
-//
-//     enum HealthKitSetupError: Error {
-//        case dataIsNotAvailable, deviceNotAvailable
-//
-//    }
-//
-//    // HealthKit Status
-//    static var notWorking: Bool = true
-//
-//
-//    // ask for permission for every data type that we want to read and ask permissions for writing the samples.
-//    class func getAuthorization(completion: @escaping (Bool, Error?) -> Void) {
-//
-//        guard HKHealthStore.isHealthDataAvailable() else {
-//            completion(false, HealthKitSetupError.deviceNotAvailable)
-//            return
-//        }
-//
-//        guard
-//            let water = HKObjectType.quantityType(forIdentifier: .dietaryWater),
-//            let dob = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
-//            let sex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
-//            let bloodType = HKObjectType.characteristicType(forIdentifier: .bloodType)
-//        else {
-//            completion(false, HealthKitSetupError.dataIsNotAvailable)
-//
-//            return
-//        }
-//
-//        let writing: Set<HKSampleType> = [water]
-//        let reading: Set<HKObjectType> = [water, dob, sex, bloodType]
-//
-//        HKHealthStore().requestAuthorization(toShare: writing, read: reading, completion: completion)
-//        self.notWorking = false
-//
-//
-//    }
-//    // writing water to HealthKit
-//    class func writeWater(amount: Double) {
-//        guard let waterType = HKSampleType.quantityType(forIdentifier: .dietaryWater) else {
-//                print("Sample type not available")
-//                return
-//            }
-//
-//            let waterQuantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: amount)
-//            let today = Date()
-//            let waterQuantitySample = HKQuantitySample(type: waterType, quantity: waterQuantity, start: today, end: today)
-//
-//            HKHealthStore().save(waterQuantitySample) { (success, error) in
-//                print("HK write finished - success: \(success); error: \(error)")
-//
-//            }
-//    }
-//
-//    // Checking authorization status
-//    class func checkAuthorization() {
-//        if self.notWorking  {
-//            HealthKitSetup.getAuthorization { (authorized, error) in
-//                guard authorized else {
-//                    let message = "authorized failed"
-//                    if let error = error {
-//                        print("\(message) reason \(error)")
-//                    }
-//                    return
-//                }
-//                print("HealthKit authorized successfuly")
-//            }
-//        }
-//    }
-//
-////    class func readCharacteristicsData() {
-////        let store = HKHealthStore()
-////        do {
-////            let dobComponents = try store.dateOfBirthComponents()
-////            let sex = try store.biologicalSex().biologicalSex
-////
-////            DispatchQueue.main.async {
-////                self.dobLabel.text = "DOB: \(dobComponents.day!)/\(dobComponents.month!)/\(dobComponents.year!)"
-////                self.sexLable.text = "Sex: \(sex.rawValue)"
-////            }
-////        } catch {
-////            print("Something went wrong: \(error)")
-////        }
-////    }
-//}
--- WaterTracker/WaterTracker/Model/WaterModel.swift
+++ WaterTracker/WaterTracker/Model/WaterModel.swift
@@ -14,25 +14,6 @@ class WaterModel {
     
     
     
-    // writing water to HealthKit
-    func writeWater(amount: Double) {
-        guard let waterType = HKSampleType.quantityType(forIdentifier: .dietaryWater) else {
-                print("Sample type not available")
-                return
-            }
-            
-            let waterQuantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: amount)
-            let today = Date()
-            let waterQuantitySample = HKQuantitySample(type: waterType, quantity: waterQuantity, start: today, end: today)
-            
-        store.save(waterQuantitySample) { (success, error) in
-                print("HK write finished - success: \(success); error: \(error)")
-                
-            }
-    }
-    
-    
-    
    
     
     
