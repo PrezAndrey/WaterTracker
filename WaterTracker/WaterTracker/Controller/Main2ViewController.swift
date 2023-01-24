a839a4349c95a69a1d7e5ce241d9c1482c1a63e8
Andrey Prez <aw06.1994@yandex.ru>
2023-01-24 13:13:33 +0800

    Add water View with buttons created

--- WaterTracker/WaterTracker/Controller/Main2ViewController.swift
+++ WaterTracker/WaterTracker/Controller/Main2ViewController.swift
@@ -11,6 +11,7 @@ class Main2ViewController: UIViewController {
     
     @IBOutlet weak var viewForTabBar: UIView!
     
+    @IBOutlet weak var addWaterView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTabBar()
@@ -18,8 +19,23 @@ class Main2ViewController: UIViewController {
     
     func configureTabBar() {
         viewForTabBar.layer.cornerRadius = viewForTabBar.frame.size.height / 2
+        addWaterView.layer.cornerRadius = 30
         
     }
 
     
+    @IBAction func addWater100(_ sender: Any) {
+        print("__________ 100 ml of water is added _________")
+    }
+    
+    @IBAction func addWater250(_ sender: Any) {
+        print("__________ 250 ml of water is added _________")
+    }
+    
+    @IBAction func deleteAmount(_ sender: Any) {
+        print("__________ last water amount deleted _________")
+    }
+    @IBAction func addCustomAmount(_ sender: Any) {
+        print("__________ add custom water amount _________")
+    }
 }
