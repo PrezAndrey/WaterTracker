84894b399146ccc3d70292de413bfa999633874e
Andrey Prez <aw06.1994@yandex.ru>
2023-01-28 20:47:52 +0800

    Extra settings screen added

---
+++ WaterTracker/WaterTracker/Controller/ExtraSettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExtraSettingsViewController.swift
+//  WaterTracker
+//
+//  Created by Андрей  on 28.01.2023.
+//
+
+import UIKit
+
+class ExtraSettingsViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
